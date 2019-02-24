3147f7e9cab9576bca53e6d652ddef90c46ab1cf
Айдари Карим <karimaidariaskm@yandex.ru>
2019-02-25 01:38:11 +0300

    change db settings

--- hairbrush/settings.py
+++ hairbrush/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'hairbrush.wsgi.application'
 # }
 
 
-import dj-database-url
+import dj_database_url
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
